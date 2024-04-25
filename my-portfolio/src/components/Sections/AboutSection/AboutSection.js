486db3d7ec0a6246d3ffcadaa49b74ae79175039
PatF3lix <pat_pg@hotmail.fr>
2024-04-25 17:55:21 +0100

    added a showMore btn and more text content to the about me section

--- my-portfolio/src/components/Sections/AboutSection/AboutSection.js
+++ my-portfolio/src/components/Sections/AboutSection/AboutSection.js
@@ -1,36 +1,41 @@
 import "./about-section.css";
+import { useState } from "react";
 import ProfessionalSkillCard from "../../ProfesionalSkillCard/ProfesionalSkillCard";
 import PersonalSkillCard from "../../PersonalSkillCard/PersonalSkillCard";
+import aboutMeText from "../../../data/AboutMeText";
+import Btn from "../../Reusable/Btn/Btn";
 
 const AboutSection = () => {
+  const [displayText, setDisplayText] = useState(false);
+
   return (
     <section className="about-section">
       <div className="about-me">
         <h1 className="about-me-title">About Me</h1>
         <div className="about-me-content">
           <h2 className="about-me-intro">Hey there, I&apos;m Patrick,</h2>
-          <p className="about-me-description">
-            A passionate self-taught programmer with three years of intense
-            learning and exploration under my belt.
-          </p>
-          <p className="about-me-description">
-            My journey into the world of coding began with a thirst for
-            knowledge and a drive to understand the intricacies of technology.
-          </p>
-          <p className="about-me-description">
-            Although I started my journey solo, I recognized the value of
-            structured learning and decided to further hone my skills by
-            attending a full-stack development course in Montreal.
-          </p>
-          <p className="about-me-description">
-            Immersing myself in a year-long program, I delved deep into the
-            world of web development, mastering various languages and frameworks
-            along the way.
-          </p>
-          <p className="about-me-description">
-            But my thirst for knowledge didn&apos;t stop there...
-          </p>
+          {displayText
+            ? aboutMeText.map((paragraph) => (
+                <p key={paragraph.id} className="about-me-description">
+                  {paragraph.text}
+                </p>
+              ))
+            : aboutMeText
+                .filter(function (parapgrah) {
+                  return parapgrah.id <= 5;
+                })
+                .map((paragraph) => (
+                  <p key={paragraph.id} className="about-me-description">
+                    {paragraph.text}
+                  </p>
+                ))}
         </div>
+        <Btn
+          classes={"btn btn-placement-right"}
+          clickAction={() => setDisplayText((visibility) => !visibility)}
+        >
+          Show More
+        </Btn>
       </div>
       <div className="about-me-img">
         <div className="profile-img" />
