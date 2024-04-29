b4cb202d3dcb570988879fa0b8ba1e276cc3ff06
PatF3lix <pat_pg@hotmail.fr>
2024-04-29 15:03:03 +0100

    submit handler in contact section is implemented

--- my-portfolio/src/components/Sections/ContactsSection/ContactsSection.js
+++ my-portfolio/src/components/Sections/ContactsSection/ContactsSection.js
@@ -7,9 +7,31 @@ const ContactsSection = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(fullName, email, message);
+    const data = {
+      fullName,
+      email,
+      message,
+    };
+
+    //send data to backend
+    try {
+      const response = await fetch("/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      //here a message should be sent to the user alerting him that the message was sent
+      //successfully
+      console.log(result);
+    } catch (err) {
+      //here a message should be sent to the user alerting him that the message was not sent
+      console.log(err);
+    }
   };
 
   return (
@@ -22,13 +44,14 @@ const ContactsSection = () => {
         </p>
         <div className="contact-form-container">
           <form>
-            <label htmlFor="name">FullName</label>
+            <label htmlFor="name">Full Name</label>
             <input
               value={fullName}
               onChange={(e) => setfullName(e.currentTarget.value)}
               type="text"
               name="name"
               id="name"
+              placeholder="Full Name"
             />
             <label htmlFor="email">Email</label>
             <input
@@ -37,6 +60,7 @@ const ContactsSection = () => {
               type="email"
               name="email"
               id="email"
+              placeholder="example@gmail.com"
             />
             <label htmlFor="message">Your message</label>
             <textarea
@@ -46,6 +70,7 @@ const ContactsSection = () => {
               rows="10"
               value={message}
               onChange={(e) => setMessage(e.currentTarget.value)}
+              placeholder="Your message goes here..."
             ></textarea>
             <br />
             <Btn clickAction={handleSubmit} classes={"btn"}>
