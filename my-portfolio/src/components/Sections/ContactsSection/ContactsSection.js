c42a314aed26c26565e39794ecdd9584d1d70bf8
PatF3lix <pat_pg@hotmail.fr>
2024-04-30 12:21:55 +0100

    styled the contact form inputs, labels , error messages and btn

--- my-portfolio/src/components/Sections/ContactsSection/ContactsSection.js
+++ my-portfolio/src/components/Sections/ContactsSection/ContactsSection.js
@@ -1,7 +1,7 @@
-import "./contacts-section.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
 import { ToastContainer, toast } from "react-toastify";
+import * as Yup from "yup";
+import "./contacts-section.css";
 import "react-toastify/dist/ReactToastify.css";
 
 const sendDataToBackend = async (values) => {
@@ -20,80 +20,94 @@ const sendDataToBackend = async (values) => {
   }
 };
 
-const ContactsSection = () => {
-  const handleSubmit = async (values) => {
-    try {
-      await sendDataToBackend(values);
-      toast.success("Message sent successfully!");
-    } catch (err) {
-      toast.error("Failed to send message. Please try agian.");
-    }
-  };
+const handleSubmit = async (values) => {
+  try {
+    await sendDataToBackend(values);
+    toast.success("Message sent successfully!");
+  } catch (err) {
+    toast.error("Failed to send message. Please try agian.");
+  }
+};
 
+const ContactsSection = () => {
   return (
     <section className="contact-section">
-      <div className={"contact-container"}>
-        <h1 className={"contact-container-title"}>Contact</h1>
-        <p className={"contact-container-intro"}>
+      <div className={"contact-container-intro"}>
+        <h1 className={"contact-title"}>Contact</h1>
+        <p className={"contact-text"}>
           Feel free to Contact me by submitting the form below and I will get
           back to you as soon as possible
         </p>
-        <div className="contact-form-container">
-          <ToastContainer
-            position="top-center"
-            gutter={12}
-            containerStyle={{ margin: "8px" }}
-            toastOptions={{
-              success: {
-                duration: 3000,
-              },
-              error: {
-                duration: 5000,
-              },
-              style: {
-                fontSize: "16px",
-                maxWidth: "500px",
-                padding: "16px 24px",
-              },
-            }}
-          />
-          <Formik
-            initialValues={{ fullName: "", email: "", message: "" }}
-            validationSchema={Yup.object().shape({
-              fullName: Yup.string()
-                .matches(/^[a-zA-Z ]*$/, "Full name must not contain numbers")
-                .required("Full name is required"),
-              email: Yup.string()
-                .email("Invalid email address")
-                .required("Email is required"),
-              message: Yup.string().required("Message is required"),
-            })}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <div>
-                  <label htmlFor="fullName">Full Name</label>
-                  <Field type="text" name="fullName" />
-                  <ErrorMessage name="fullName" component="div" />
-                </div>
-                <div>
-                  <label htmlFor="email">Email</label>
-                  <Field type="email" name="email" />
-                  <ErrorMessage name="email" component="div" />
-                </div>
-                <div>
-                  <label htmlFor="message">Message</label>
-                  <Field as="textarea" name="message" />
-                  <ErrorMessage name="message" component="div" />
-                </div>
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-          </Formik>
-        </div>
+      </div>
+      <div className="contact-form-container">
+        <ToastContainer
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+            },
+          }}
+        />
+        {/* Form fields validation */}
+        <Formik
+          initialValues={{ fullName: "", email: "", message: "" }}
+          validationSchema={Yup.object().shape({
+            fullName: Yup.string()
+              .matches(/^[a-zA-Z ]*$/, "Full name must not contain numbers")
+              .required("Full name is required"),
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Email is required"),
+            message: Yup.string().required("Message is required"),
+          })}
+          onSubmit={handleSubmit}
+        >
+          {/* Form component*/}
+          {({ isSubmitting }) => (
+            <Form className="contact-form">
+              <div className="form-field">
+                <label htmlFor="fullName">Full Name</label>
+                <Field type="text" name="fullName" />
+                <ErrorMessage
+                  name="fullName"
+                  component="div"
+                  className="error-text"
+                />
+              </div>
+              <div className="form-field">
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="error-text"
+                />
+              </div>
+              <div className="form-field">
+                <label htmlFor="message">Message</label>
+                <Field as="textarea" name="message" />
+                <ErrorMessage
+                  name="message"
+                  component="div"
+                  className="error-text-textarea"
+                />
+              </div>
+              <button className="btn" type="submit" disabled={isSubmitting}>
+                {isSubmitting ? "Sending Message" : "Submit"}
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </section>
   );
